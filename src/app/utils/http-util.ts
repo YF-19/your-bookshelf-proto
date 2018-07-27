8e709db3ae98a52adc957bd63fe6d4016bc749f4
def <dkeiflylf@gmail.com>
2018-07-27 10:48:29 +0900

    Modify settings for deploy

--- src/app/utils/http-util.ts
+++ src/app/utils/http-util.ts
@@ -2,7 +2,7 @@ import { HttpHeaders } from '@angular/common/http';
 
 export class HttpUtil {
     // static API_BASE_URL = 'http://localhost:3000';
-    static API_BASE_URL = 'https://agile-everglades-18086.herokuapp.com/';
+    static API_BASE_URL = 'https://agile-everglades-18086.herokuapp.com';
 
     static makeHttpOptions(): any {
         const headersObj = Object.assign(
